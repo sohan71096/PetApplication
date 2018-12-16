cf59ffedc94f413177aa84f99a09eee9437834a8
sohan71096 <45695633+sohan71096@users.noreply.github.com>
2018-12-16 23:44:05 +0530

    Add files via upload

---
+++ PetApplication/bin/Instructions.sql
@@ -0,0 +1,3 @@
+create table pet_owner_details( owner_id varchar2(5), owner_name varchar2(20),owner_ph_no varchar2(10),owner_age varchar2(2),vaccine_date varchar2(10),next_vaccination_date varchar2(10));
+
+create sequence pet_set start with 10000;
